d4d3194607e24b493cd45fb342e6d5782edda045
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-01-17 20:34:45 -0300

    create Category controller

--- src/main/java/br/com/diegocoding/dscatalog/controllers/CategoryController.java
+++ src/main/java/br/com/diegocoding/dscatalog/controllers/CategoryController.java
@@ -1,8 +1,10 @@
 package br.com.diegocoding.dscatalog.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(name = "/categories")
 public class CategoryController {
 
 }
