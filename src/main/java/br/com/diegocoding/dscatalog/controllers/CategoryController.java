f4e971756d56dd6eac6e49d22e9f44d4e57fbe92
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2023-01-18 15:03:11 -0300

    configuration endpoint findall

--- src/main/java/br/com/diegocoding/dscatalog/controllers/CategoryController.java
+++ src/main/java/br/com/diegocoding/dscatalog/controllers/CategoryController.java
@@ -1,10 +1,25 @@
 package br.com.diegocoding.dscatalog.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(name = "/categories")
-public class CategoryController {
+import br.com.diegocoding.dscatalog.entities.Category;
+import br.com.diegocoding.dscatalog.services.CategoryService;
 
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+	@Autowired
+	CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 }
---
+++ src/main/java/br/com/diegocoding/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package br.com.diegocoding.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.diegocoding.dscatalog.entities.Category;
+import br.com.diegocoding.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<Category> findAll() {
+		return this.repository.findAll();
+	}
+}
